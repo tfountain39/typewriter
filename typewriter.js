38839314605e75435fb5ba8caa077a0dee6add84
Todd <todd.fountain39@gmail.com>
2023-10-25 21:10:34 -0500

    Update of typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,23 @@
+// #typewriter
+
+// Sentence to print
 const sentence = "hello there from lighthouse labs";
+// Initialize index variable as counter
 let index = 0
 
+// Loop each index
 for (const char of sentence) {
+  // 
   setTimeout(function() {
+    // use process.stdout.write on each character to have them appear in the same line
     process.stdout.write(char)
+    // Delay by 50 ms per character
   }, index * 50);
+  // increment
   index++;  
 }
 
+// After all iterations are done, add a newline based on delay
 setTimeout(function() {
   process.stdout.write('\n');
 }, sentence.length * 50);
